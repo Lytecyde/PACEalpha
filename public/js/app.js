a8d1924dbc9406f05518d6c0f37c880db1e8fe1d
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2020-08-29 05:39:51 +0300

    added assets;spy moves

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,149 @@
-let psyduck;
+var player;
+
+
+var SceneA = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function SceneA ()
+    {
+        Phaser.Scene.call(this, { key: 'sceneA' });
+    },
+
+    preload: function ()
+    {
+
+        this.load.image('face', '../assets/sprites/spy_gray.png');
+    },
+
+    create: function ()
+    {
+        this.add.sprite(400, 300, 'face').setAlpha(0.2);
+
+        this.input.once('pointerdown', function () {
+
+            console.log('From SceneA to SceneB');
+
+            this.scene.start('sceneB');
+
+        }, this);
+    }
+
+});
+
+var SceneB = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function SceneB ()
+    {
+        Phaser.Scene.call(this, { key: 'sceneB' });
+    },
+
+    preload: function ()
+    {
+        this.load.image('arrow', '../assets/sprites/spy_white.png');
+    },
+
+    create: function ()
+    {
+        this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5);
+
+        this.input.once('pointerdown', function (event) {
+
+            console.log('From SceneB to SceneC');
+
+            this.scene.start('sceneC');
+
+        }, this);
+    },
+
+    update: function (time, delta)
+    {
+        this.arrow.rotation += 0.01;
+    }
+
+});
+
+var SceneC = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function SceneC ()
+    {
+        Phaser.Scene.call(this, { key: 'sceneC' });
+    },
+
+    preload: function ()
+    {
+        this.load.image('spyblack', '../assets/sprites/spy_black.png');
+    },
+
+    create: function ()
+    {
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.spyblack = this.add.sprite(Phaser.Math.Between(0, 800), 300, 'spyblack');
+
+        player = this.physics.add.image(400, 300, 'spyblack');
+
+        player.setCollideWorldBounds(true);
+
+        this.input.once('pointerdown', function (event) {
+
+            console.log('From SceneC to SceneA');
+
+            this.scene.start('sceneA');
+
+        }, this);
+    },
+
+    update: function (time, delta)
+    {
+      player.setVelocity(0);
+
+      if (cursors.left.isDown)
+      {
+          player.setVelocityX(-300);
+      }
+      else if (cursors.right.isDown)
+      {
+          player.setVelocityX(300);
+      }
+
+      if (cursors.up.isDown)
+      {
+          player.setVelocityY(-300);
+      }
+      else if (cursors.down.isDown)
+      {
+          player.setVelocityY(300);
+      }
+    }
+
+});
+
+var cursors;
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: '#fff',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            debug: true
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    parent: 'phaser-example',
+    scene: [ SceneA, SceneB, SceneC ]
 };
 
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-    //psyduck mashup
-    this.load.image('psyduck', '../assets/psyduck.png');
-
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-}
-
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
-
-    psyduck = this.add.sprite(64, 64, 'psyduck');
-
-}
-
-function update() {
-  psyduck.x += 1;
-}
