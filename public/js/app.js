07850e9f41b0f0a9e28a891aa7dfdfdb1ec8ce84
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2020-08-29 10:52:52 +0300

    more spy assets; added spy movement

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ var SceneA = new Phaser.Class({
 
     create: function ()
     {
-        this.add.sprite(400, 300, 'face').setAlpha(0.2);
+        this.add.sprite(400, 300, 'face');
 
         this.input.once('pointerdown', function () {
 
@@ -32,7 +32,6 @@ var SceneA = new Phaser.Class({
     }
 
 });
-<<<<<<< HEAD
 
 var SceneB = new Phaser.Class({
 
@@ -83,78 +82,24 @@ var SceneC = new Phaser.Class({
 
     preload: function ()
     {
-        this.load.image('spyblack', '../assets/sprites/spy_black.png');
-        this.load.image('spywhite', '../assets/sprites/spy_white.png');
-        this.load.image('spygray', '../assets/sprites/spy_gray.png');
+        this.load.image('spyblack-right', '../assets/sprites/spy_black.png');
+        this.load.image('spywhite-right', '../assets/sprites/spy_white.png');
+        this.load.image('spygray-right', '../assets/sprites/spy_gray.png');
+        this.load.image('spyblack-left', '../assets/sprites/spy_black_left.png');
+        this.load.image('spywhite-left', '../assets/sprites/spy_white_left.png');
+        this.load.image('spygray-left', '../assets/sprites/spy_gray_left.png');
 
-=======
-
-var SceneB = new Phaser.Class({
-
-    Extends: Phaser.Scene,
-
-    initialize:
-
-    function SceneB ()
-    {
-        Phaser.Scene.call(this, { key: 'sceneB' });
-    },
-
-    preload: function ()
-    {
-        this.load.image('arrow', '../assets/sprites/spy_white.png');
-    },
-
-    create: function ()
-    {
-        this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5);
-
-        this.input.once('pointerdown', function (event) {
-
-            console.log('From SceneB to SceneC');
-
-            this.scene.start('sceneC');
-
-        }, this);
-    },
-
-    update: function (time, delta)
-    {
-        this.arrow.rotation += 0.01;
-    }
-
-});
-
-var SceneC = new Phaser.Class({
-
-    Extends: Phaser.Scene,
-
-    initialize:
-
-    function SceneC ()
-    {
-        Phaser.Scene.call(this, { key: 'sceneC' });
-    },
-
-    preload: function ()
-    {
-        this.load.image('spyblack', '../assets/sprites/spy_black.png');
->>>>>>> a8d1924dbc9406f05518d6c0f37c880db1e8fe1d
     },
 
     create: function ()
     {
         cursors = this.input.keyboard.createCursorKeys();
 
-        this.spyblack = this.add.sprite(Phaser.Math.Between(0, 800), 300, 'spyblack');
+        this.spyblack = this.add.sprite(Phaser.Math.Between(0, 800), 300, 'spyblack-right');
 
-<<<<<<< HEAD
-        this.spywhite = this.add.sprite(Phaser.Math.Between(0, 800), 300, 'spywhite');
+        this.spywhite = this.add.sprite(Phaser.Math.Between(0, 800), 300, 'spywhite-right');
 
-        player = this.physics.add.image(400, 300, 'spygray');
-=======
-        player = this.physics.add.image(400, 300, 'spyblack');
->>>>>>> a8d1924dbc9406f05518d6c0f37c880db1e8fe1d
+        player = this.physics.add.image(400, 300, 'spygray-right');
 
         player.setCollideWorldBounds(true);
 
@@ -173,10 +118,14 @@ var SceneC = new Phaser.Class({
 
       if (cursors.left.isDown)
       {
+          player.toggleFlipX();
+          //player = this.physics.add.sprite(player.x, player.y, 'spygray-left');
           player.setVelocityX(-300);
       }
       else if (cursors.right.isDown)
       {
+          player.toggleFlipX();
+          //player = this.physics.add.sprite(player.x, player.y, 'spygray-right');
           player.setVelocityX(300);
       }
 
@@ -199,14 +148,11 @@ var config = {
     width: 800,
     height: 600,
     backgroundColor: '#fff',
+    baseURL: 'http://localhost:8080/',
     physics: {
         default: 'arcade',
         arcade: {
-<<<<<<< HEAD
             debug: false
-=======
-            debug: true
->>>>>>> a8d1924dbc9406f05518d6c0f37c880db1e8fe1d
         }
     },
     parent: 'phaser-example',
